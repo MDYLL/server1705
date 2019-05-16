580d05dd92b4c0c2864294d4cd786106e98fac83
Dmitri Martynov <mdylll@mail.ru>
2019-05-16 22:40:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ let players = 0;
 let gameID = 0;
 let flag = false;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 function game() {
     return new Promise(((resolve, reject) => {
